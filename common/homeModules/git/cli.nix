c9cd31756d0973f470b4c16ab57e17df7ca9059a
vox-eternal <voxxus@proton.me>
2025-08-14 01:22:02 +0000

    signing test

--- common/homeModules/git/cli.nix
+++ common/homeModules/git/cli.nix
@@ -14,7 +14,23 @@
       lfs = {
         enable = true;
       };
+      signing = {
+        key = "d2b664b9972e8e321d3ba0df4ef7aa1a3927ec28ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIByAlyhN/aO8uLlv2jrp+eFJDB7142c7Wb07tyvH+8Xs voxxus@izar";
+        signByDefault = true;
+      };
       extraConfig = {
+        gpg = {
+          format = "ssh";
+        };
+        user = {
+          signingkey = "~/.ssh/id_ed25519";
+        };
+        commit = {
+          gpgsign = true;
+        };
+        push = {
+          autoSetupRemote = true;
+        };
         init = {
           defaultBranch = "main";
         };
