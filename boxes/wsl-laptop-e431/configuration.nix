4a7080c8eba718876142e671560306b5620c5506
vox-eternal <voxxus@proton.me>
2025-08-14 20:15:40 -0400

    tz update

--- boxes/wsl-laptop-e431/configuration.nix
+++ boxes/wsl-laptop-e431/configuration.nix
@@ -26,6 +26,8 @@
     };
   };
 
+  time.timeZone = "America/New_York";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
